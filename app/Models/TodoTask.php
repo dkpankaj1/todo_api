3b054caa9896c43127f8e0ab8e230dee5f301a98
Dipankar kumar pankaj <dipankarpankaj38@gmail.com>
2026-07-01 18:06:46 +0530

    fixes

--- app/Models/TodoTask.php
+++ app/Models/TodoTask.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class TodoTask extends Model
+class ToDoTask extends Model
 {
     protected $fillable = [
         'title',
