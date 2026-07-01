16069cf912a941085ff300d6e4e3c079cd34174a
Dipankar kumar pankaj <dipankarpankaj38@gmail.com>
2026-07-01 17:46:03 +0530

    fixes

--- app/Jobs/LogCompletedTask.php
+++ app/Jobs/LogCompletedTask.php
@@ -2,7 +2,7 @@
 
 namespace App\Jobs;
 
-use App\Models\TodoTask;
+use App\Models\ToDoTask;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Log;
@@ -15,7 +15,7 @@ class LogCompletedTask implements ShouldQueue
      * Create a new job instance.
      */
     public function __construct(
-        public TodoTask $task,
+        public ToDoTask $task,
     ) {}
 
     /**
